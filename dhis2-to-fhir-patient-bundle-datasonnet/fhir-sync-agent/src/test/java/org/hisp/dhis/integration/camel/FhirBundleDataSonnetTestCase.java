3c9e55c7f639ce8a9b3e8a8d272ced682f4ea199
JohanGHole <johan.hole@dhis2.org>
2025-10-27 12:38:52 +0100

    fix: SQ issues

--- dhis2-to-fhir-patient-bundle-datasonnet/fhir-sync-agent/src/test/java/org/hisp/dhis/integration/camel/FhirBundleDataSonnetTestCase.java
+++ dhis2-to-fhir-patient-bundle-datasonnet/fhir-sync-agent/src/test/java/org/hisp/dhis/integration/camel/FhirBundleDataSonnetTestCase.java
@@ -41,12 +41,9 @@ import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.shortThat;
 
 public class FhirBundleDataSonnetTestCase {
 
